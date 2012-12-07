37a512bdfeb15ac3f35622f3a6857e3938376a88
Mattia Belletti <mattia.belletti@gmail.com>
2012-12-07 10:58:14 +0100

    Beginning of test suite
    Just a basic test method to check it's working.

---
+++ RedParserTests/BasicTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedParser;
+using System.IO;
+
+namespace RedParserTests
+{
+    [TestClass]
+    public class BasicTests: TestBase
+    {
+        [TestMethod]
+        public void ReadMinimalXml()
+        {
+            parse<Empty>("BasicTests_ReadMinimalXml.xml");
+        }
+    }
+}
---
+++ RedParserTests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RedParser;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedParserTests
+{
+    public class TestBase
+    {
+        protected T parse<T>(string filename)
+        {
+            var d = new HumanXmlDeserializer();
+            var ret = d.Deserialize<T>(
+                Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    filename));
+            Assert.IsNotNull(ret);
+            return ret;
+        }
+    }
+}
